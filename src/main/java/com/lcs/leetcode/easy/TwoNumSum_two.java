0ffe457f64d36489a8e8c580bef011e22619195a
chenshuo <liuchenshuo@126.com>
2020-05-02 11:35:10 +0800

    first commit

---
+++ src/main/java/com/lcs/leetcode/easy/TwoNumSum_two.java
@@ -0,0 +1,23 @@
+package com.lcs.leetcode.easy;
+
+public class TwoNumSum_two {
+    public static void main(String[] args) {
+        int[] reNums = twoSum(new int[]{2, 7, 11, 15},9);
+        System.out.println(reNums[0] + "-" +reNums[1]);
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        if (numbers == null) return null;
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return null;
+    }
+}
